df7f0cef09eff0c6f36864f99924930ec860e30d
Romain Deheele <romaindeheele@gmail.com>
2021-02-06 23:08:56 +0100

    [UPD] mapping sale_type

--- specific/models/sale.py
+++ specific/models/sale.py
@@ -5,6 +5,21 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.model
-    def _default_type_id(self):
-        return False
+    @api.depends("partner_id", "company_id")
+    def _compute_sale_type_id(self):
+        for record in self:
+            if not record.partner_id:
+                record.type_id = self.env["sale.order.type"].search(
+                    [("company_id", "in", [self.env.company.id, False])], order='id desc', limit=1
+                )
+            else:
+                sale_type = (
+                    record.partner_id.with_context(
+                        force_company=record.company_id.id
+                    ).sale_type
+                    or record.partner_id.commercial_partner_id.with_context(
+                        force_company=record.company_id.id
+                    ).sale_type
+                )
+                if sale_type:
+                    record.type_id = sale_type
