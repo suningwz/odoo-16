3487344e56a29c7f71364ef02d291f9de4ff97b6
Romain Deheele <romaindeheele@gmail.com>
2021-02-21 20:51:32 +0100

    [UPD] add dependence

--- specific/__manifest__.py
+++ specific/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Specific",
     "version": "13.0.1.0.0",
     "license": "AGPL-3",
-    "depends": ["sale_order_type"],
+    "depends": ["sale_order_type", "sale_automatic_workflow"],
     "data": ['views/sale_order_type_view.xml'],
     "installable": True,
 }
